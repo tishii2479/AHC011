63d15d9d5f748cf4caefa5e5b1dc88b7f23cabe3
Tatsuya Ishii <tishii2479@gmail.com>
2022-05-30 15:31:03 +0900

    moving

--- AHC011/TreeConstructor.swift
+++ AHC011/TreeConstructor.swift
@@ -11,43 +11,40 @@ final class TreeConstructorV1: TreeConstructor {
     }
     
     func construct() -> Board {
-        var allTiles: [Tile] = initialBoard.allTiles
-        allTiles.shuffle()
-        var used: [Bool] = [Bool](repeating: false, count: allTiles.count)
+        var tileCounts: [Int] = initialBoard.countTiles()
         let resultBoard = Board(n: initialBoard.n)
         
         let start = Pos(x: 3, y: 3)
-        // O(n)
-        resultBoard.place(at: start, tile: allTiles.removeFirst(), force: true)
+        resultBoard.place(at: start, tile: Tile(rawValue: 15)!, force: true)
+        tileCounts[15] -= 1
         
         var allPositions: [Pos] = [start]
         var seen: [[Bool]] = [[Bool]](repeating: [Bool](repeating: false, count: resultBoard.n), count: resultBoard.n)
+        
+        // just for first case
+        seen[start.y][start.x] = true
         
         for _ in 0 ..< 1000 {
             guard let pos = allPositions.first else {
                 IO.log("No more positions, pos count: \(allPositions.count)", type: .warn)
                 break
             }
-
-            // just for first case
-            seen[pos.y][pos.x] = true
             
             for dir in Dir.all {
                 let nextPos = pos + dir.pos
                 guard nextPos.isValid(boardSize: resultBoard.n) else { continue }
-                for (i, tile) in allTiles.enumerated() {
-                    guard !used[i] else { continue }
-                    // don't allow to end create tree
-                    if tile.rawValue.nonzeroBitCount == 1 && allPositions.count == 1 {
-                        continue
-                    }
-                    if resultBoard.isPlaceable(at: nextPos, tile: tile) {
+
+                for _ in 0 ..< 100 {
+                    let rawValue = Int.random(in: 1..<16)
+                    if let tile = Tile(rawValue: rawValue),
+                       tile.isDir(dir: dir.rev) && tileCounts[rawValue] > 0,
+                       resultBoard.isPlaceable(at: nextPos, tile: tile) {
                         resultBoard.place(at: nextPos, tile: tile, force: true)
+                        tileCounts[rawValue] -= 1
                         if !seen[nextPos.y][nextPos.x] {
                             // prevent appending newPos again
                             seen[nextPos.y][nextPos.x] = true
                             allPositions.append(nextPos)
-                            used[i] = true
                         }
                         break
                     }
@@ -56,10 +53,14 @@ final class TreeConstructorV1: TreeConstructor {
 
             // O(n)
             allPositions.removeFirst()
-            allTiles.removeFirst()
         }
         
+        // find muda tile, and replace if possible
+        
+        // replace tile that will extend the tree
+        
         resultBoard.log()
+        IO.log(tileCounts)
         return resultBoard
     }
 }
