503b562dd83256df0a1ab19b41bb876f5fbe7d8f
Tatsuya Ishii <tishii2479@gmail.com>
2022-06-01 16:48:51 +0900

    wip

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ echo "[LOG] Compile out/main.swift..."
 swiftc out/main.swift -o out/main.o
 
 #echo "[LOG] Compile main.cpp..."
-
 #g++ -std=c++17 main.cpp -o out/main.o
 
 echo "[LOG] Compile done. -> out/main.o"
